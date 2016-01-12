61e8ed3131b0cbf8cb770f4c4fef3b31d44b78b2
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 18:01:17 -0800

    Working on further study - Updates to Random Restaurants

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,4 +1,9 @@
+import random
+
 restaurant_ratings = {}
+
+# Check to see if this goes in a better place
+restaurant_names = None 
 
 def sort_restaurant_ratings(filename):
     """Takes in restaurant ratings file and returns alphabetized list."""
@@ -16,6 +21,8 @@ def sort_restaurant_ratings(filename):
 
     restaurant_names = restaurant_ratings.keys()
     sorted_restaurant_names = sorted(restaurant_names)
+
+    update_restaurant_ratings()
 
     for restaurant in sorted_restaurant_names:
         print restaurant + " is rated at " + str(restaurant_ratings[restaurant]) + "."
@@ -36,4 +43,35 @@ def get_user_rating():
     restaurant_ratings[new_restaurant] = new_score
 
 
+def update_restaurant_ratings():
+    """Prompts user to update ratings for a random restaurant.
+
+    Takes in a list from which to choose x number of random restaurants to update."""
+
+    random_restaurant = random.choice(restaurant_names)
+    print random_restaurant
+    # user_name = raw_input("Hello, there! What's your name?\n>> ")
+    # user_rating = int(raw_input("Please update this restaurant's rating from 1 (lowest) to 5 (highest):\n>> "))
+
+
 sort_restaurant_ratings("scores.txt")
+
+
+
+# Choose a random restaurant from the list in the file
+# import random to select random key from dict
+
+# Tell the user the chosen restaurant and its rating
+# print key and rating for randomly generated KV pair
+
+# Ask the user what the new rating should be
+# raw input, ask for new rating for random restaurant
+
+# Update the rating
+# Overwrite rating for random rest.
+
+# Choose another restaurant, repeat steps 2-4
+# All of this should be a while loop while not 'q'
+
+# Continue until the user types q.
+
